156458c3b06d9b03e1eac7fb14eeaaf96e5dcb1a
cheujona853 <cheujona853@2260I-L42-74>
2022-05-18 09:48:43 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lesson_3
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
